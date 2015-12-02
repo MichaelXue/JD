d74a4f81bf74ecc1d21d2f037a648bbfcbaeeff2
MichaelXue <862463500xf@gmail.com>
2015-12-02 11:44:47 +0800

    JD

--- js/index.js
+++ js/index.js
@@ -70,14 +70,14 @@ $(function() {
 
 //轮播
 jQuery(function($) {
-    $('#demo1').slideBox();
+    /*$('#demo1').slideBox();
     $('#demo2').slideBox({
         direction: 'top', //left,top#方向
         duration: 0.3, //滚动持续时间，单位：秒
         easing: 'linear', //swing,linear//滚动特效
         delay: 5, //滚动延迟时间，单位：秒
         startIndex: 1 //初始焦点顺序
-    });
+    });*/
     $('#demo3').slideBox({
         duration: 0.3, //滚动持续时间，单位：秒
         easing: 'linear', //swing,linear//滚动特效
